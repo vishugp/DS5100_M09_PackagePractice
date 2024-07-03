84b274bdcc442e75a72e7847f3d48add8d8a7488
Vishwanath Guruvayur <vishvesh2704@gmail.com>
2024-07-03 16:33:18 -0400

    Updated Setup File

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 setup(
     name = 'PracticePackage',
     version='0.1',
-    description='Practice Package for M08 DS5100',
-    url='https://github.com/vishugp/M08_PackagePractice',
+    description='Practice Package for M09 DS5100',
+    url='https://github.com/vishugp/DS5100_M09_PackagePractice',
     author='Vishwanath Guruvayur',
     author_email='qtf7du@virginia.edu',
     license = 'MIT',
